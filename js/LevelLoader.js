61d48b27b69e19b138a78514732aa8b6239943c4
Greg Tatum <tatum.creative@gmail.com>
2014-12-13 15:07:08 -0600

    Analytics and meta stuff

--- js/LevelLoader.js
+++ js/LevelLoader.js
@@ -6,12 +6,17 @@ var currentPoem = null;
 
 window.LevelLoader = function( name ) {
 	
+	//Track this, but exclude first load
+	if( currentLevel ) {
+		_gaq.push(['_trackPageview', '/polar/'+name]);
+	}
+	
 	if(currentPoem) currentPoem.destroy();
 	
 	currentLevel = levels[name];
 	currentPoem = new Poem( currentLevel );
 	window.poem = currentPoem;
-
+	
 };
 	
 module.exports = LevelLoader;
